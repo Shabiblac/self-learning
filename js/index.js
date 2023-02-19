72628e4ebd9215d48c861db1a163bad6f3cccccc
Shabiblac <adedoyiashabi@gmail.com>
2023-02-20 00:39:22 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector(".nav-ul");
+
+menu.onclick = ()=>{
+    menu.classList.toggle("bx-x");
+    nav-ul.classList.toggle("open");
+}
